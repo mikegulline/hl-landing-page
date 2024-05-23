ec873ba0ed141b6431e2a75550feedad97189bf6
Mike Gulline <mike@mikegulline.com>
2024-05-23 14:47:51 -0700

    better banner styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     <div className='background bg-gradient-to-t from-purple-600 to-purple-950 text-white'>
       <div className='container mx-auto py-10 lg:py-20'>
         <div className='px-5'>
-          <div className='flex items-center'>
+          <div className='flex flex-col sm:flex-row items-center'>
             <div className='sm:pl-6 lg:pl-10 2xl:pl-14 sm:border-l-8 border-solid border-purple-300 w-full max-w-xl lg:max-w-xl xl:max-w-2xl 2xl:max-w-4xl'>
-              <h1 className=' leading-tight sm:leading-tight md:leading-tight lg:leading-tight xl:leading-tight 2xl:leading-tight text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold '>
+              <h1 className=' leading-tight sm:leading-tight md:leading-tight lg:leading-tight xl:leading-tight 2xl:leading-tight mb-10 sm:mb-0 text-center sm:text-left text-4xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold '>
                 3 Reasons Why <br />
                 Bliss Bombs™ Blow <br />
                 Away ALL Hemp <br />
@@ -18,7 +18,7 @@ export default function Home() {
             </div>
             <div className='w-full'>
               <span
-                className='mx-auto block relative max-w-24 sm:max-w-40 md:max-w-48 lg:max-w-56  xl:max-w-72 2xl:max-w-96
+                className='mx-auto block relative max-w-60 sm:max-w-40 md:max-w-48 lg:max-w-56  xl:max-w-72 2xl:max-w-96
                 '
               >
                 <Image
@@ -27,7 +27,7 @@ export default function Home() {
                   height={100}
                   alt='Bliss Bombs Gummies'
                 />
-                <div className='floating2 absolute -top-[10%] -right-[20%] w-[40%]'>
+                <div className='floating2 absolute -top-[15%] -right-[20%] w-[40%]'>
                   <Image
                     src={Gummy}
                     width={150}
@@ -46,7 +46,7 @@ export default function Home() {
                   />
                 </div>
 
-                <div className='floating3 absolute bottom-0 -right-[5%]  w-[25%]'>
+                <div className='floating3 absolute bottom-[10%] -right-[5%]  w-[25%]'>
                   <Image
                     src={Gummy}
                     width={100}
