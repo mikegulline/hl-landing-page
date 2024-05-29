c487417d534332ef1e26650282314c2a03a98b84
Mike Gulline <mike@mikegulline.com>
2024-05-29 16:55:29 -0700

    new menu

--- src/app/(infos)/layout.tsx
+++ src/app/(infos)/layout.tsx
@@ -6,49 +6,52 @@ export default function InfoLayout({
 }>) {
   return (
     <>
-      <div className='p-5 bg-fuchsia-900'>
-        <ul className='flex  justify-center items-center gap-5'>
-          <li>
-            <Link
-              href='/'
-              className=' text-white font-light block underline hover:text-fuchsia-400'
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/contact'
-              className=' text-white font-light block underline hover:text-fuchsia-400'
-            >
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/returns'
-              className=' text-white font-light block underline hover:text-fuchsia-400'
-            >
-              Returns
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/terms'
-              className=' text-white font-light block underline hover:text-fuchsia-400'
-            >
-              Terms
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/privacy'
-              className=' text-white font-light block underline hover:text-fuchsia-400'
-            >
-              Privacy
-            </Link>
-          </li>
-        </ul>
+      <div className=' bg-fuchsia-900'>
+        <div className=' mx-auto max-w-screen-md'>
+          <ul className='flex p-5 justify-center items-center gap-5'>
+            <li>
+              <Link
+                href='/'
+                className=' text-white font-light block underline hover:text-fuchsia-400'
+              >
+                Home
+              </Link>
+            </li>
+            <li className='flex-grow'></li>
+            <li>
+              <Link
+                href='/contact'
+                className=' text-white font-light block underline hover:text-fuchsia-400'
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/returns'
+                className=' text-white font-light block underline hover:text-fuchsia-400'
+              >
+                Returns
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/terms'
+                className=' text-white font-light block underline hover:text-fuchsia-400'
+              >
+                Terms
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/privacy'
+                className=' text-white font-light block underline hover:text-fuchsia-400'
+              >
+                Privacy
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
       {children}
     </>
