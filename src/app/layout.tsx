629127cf2bb1dee6d9f336e3d267b6bab0c1cec3
Mike Gulline <mike@mikegulline.com>
2024-05-30 08:06:01 -0700

    added favicon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+        <link rel='icon' href='/favicon.ico' />
         <Script
           id='tiktok-analytics'
           strategy='beforeInteractive'
